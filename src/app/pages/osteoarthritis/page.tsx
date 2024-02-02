c257bfd055e1d7befc5359768d6b834f2bd9496f
Nemidas <nehalchaudhari5302@gmail.com>
2024-02-02 13:40:37 +0530

    Mobile responsiveness improvements

--- src/app/pages/osteoarthritis/page.tsx
+++ src/app/pages/osteoarthritis/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 export default function Home(){
     return(
-        <div className="relative h-screen">
+        <div className="relative">
             <div className="container w-full px-4 md:max-w-2xl md:absolute md:p-16 md:ml-16 z-10">
                 <div className="bg-white min-h-20 md:min-w-96 md:min-h-64 md:shadow-lg rounded">
                     <h1 className="font-bold text-2xl mb-0 md:text-4xl pl-3 pt-3">Osteoarthritis Research</h1>
@@ -18,10 +18,11 @@ export default function Home(){
           sizes="100vw"
           style={{
             objectFit: 'cover',
+            objectPosition: '100% 10%',
             // height: 'calc(100vh - 96px)',
             width: '100vw',
           }}
-          className="mb-6 min-h-48"
+          className="mb-6 min-h-52"
         />
         <div className="flex flex-col mt-8 md:flex md:flex-row justify-center items-center gap-2 md:gap-8 md:p-8 ">
             <h1 className="text-lg font-medium text-blue-600 hover:bg-sky-200">Overview</h1>
--- src/app/pages/scoliosis/page.tsx
+++ src/app/pages/scoliosis/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 export default function Home(){
     return(
-        <div className="relative h-screen">
+        <div className="relative w-full">
             <div className="container w-full px-4 md:max-w-2xl md:absolute md:p-16 md:ml-16 z-10">
                 <div className="bg-white min-h-20 md:min-w-96 md:min-h-64 md:shadow-lg rounded">
                     <h1 className="font-bold text-2xl mb-0 md:text-4xl pl-3 pt-3">Scoliosis Research </h1>
@@ -9,20 +9,23 @@ export default function Home(){
                     <h1 className="text-xl md:text-2xl p-3 mb-1">Tools, resources, and support for living well with Scoliosis</h1>
                 </div>
             </div>
-            <Image
-              alt="Doctor with Xray"
-              src='/images/Condition_Hero_Grouping_Osteoarthritis.webp'
-              quality={100}
-              height={100}
-              width={100}
-              sizes="100vw"
-              style={{
-                objectFit: 'cover',
-                height: 'auto',
-                width: '100vw',
-              }}
-              className="mb-6 min-h-48"
-            />
+            <div className="">
+              <Image
+                alt="Doctor with Xray"
+                src='/images/Condition_Hero_Grouping_Osteoarthritis.webp'
+                quality={100}
+                height={100}
+                width={100}
+                sizes="100vw"
+                style={{
+                  objectFit:"cover",
+                  objectPosition: '100% 10%', 
+                  height: 'auto',
+                  width: '100vh',
+                }}
+                className="mb-6 min-h-64"
+              />
+            </div>
           <div className="flex flex-col mt-8 md:flex md:flex-row justify-center items-center gap-2 md:gap-8 md:p-8 ">
               <h1 className="text-lg text-sky-500 hover:bg-sky-200">Overview</h1>
               <h1 className="text-lg text-sky-500 hover:bg-sky-200">Symptoms</h1>
